61d8bd6d27f0d7c2d14023ba3aaae98c29d1bbe1
shahin7333 <shaparan7333@gmail.com>
2022-04-04 20:16:12 +0600

    chart added

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,9 +1,73 @@
 import React from 'react';
-
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css'
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Rechart</h1>
+            <div className='rechart'>
+
+                <LineChart className='chart'
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis yAxisId="left" />
+                    <YAxis yAxisId="right" orientation="right" />
+                    <Tooltip />
+                    <Legend />
+                    <Line yAxisId="left" type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line yAxisId="right" type="monotone" dataKey="sell" stroke="#82ca9d" />
+                    <Line yAxisId="right" type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                </LineChart>
+
+            </div>
         </div>
     );
 };
