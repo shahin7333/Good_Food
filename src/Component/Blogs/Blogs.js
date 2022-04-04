6878484aeb816a893b1f2105c804a4f214dd25e3
shahin7333 <shaparan7333@gmail.com>
2022-04-04 21:11:58 +0600

    answer-part update

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>hello from Blogs</h1>
+        <div className='ans-ques-container'>
+            <h2>This block is about question-answer.</h2>
+            <div className='answer-question'>
+                <div className='answer'>
+                    <h3>What is context API?</h3>
+                    <p>The definition of context API is, a react structure that enable you to exchange unique details and assist a solving prop-drilling from all levels of your application. The context API is a way for a React app to effectively produce global variables that can be passed around. That means context provides a way to pass data through the component tree without having to pass props down manually at every level.</p>
+                </div>
+                <div className='answer'>
+                    <h3>What is semantic tag?</h3>
+                    <p>Semantic tag is known also semantic HTML elements which are clearly describe their meaning in a human and machine readable way. It is the purpose of elements and attribute and the logical relationship between element and the attributes of those elements. Its means the HTML more comprehensible by better defines the different section and layout of web pages.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import './Dashboard.css'
 const Dashboard = () => {
     const data = [
@@ -41,32 +41,57 @@ const Dashboard = () => {
         }
     ]
     return (
-        <div>
+        <div className='chart'>
             <h1>Rechart</h1>
-            <div className='rechart'>
+            <div className='chart-container'>
+                <div>
 
-                <LineChart className='chart'
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
-                    <YAxis yAxisId="left" />
-                    <YAxis yAxisId="right" orientation="right" />
-                    <Tooltip />
-                    <Legend />
-                    <Line yAxisId="left" type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line yAxisId="right" type="monotone" dataKey="sell" stroke="#82ca9d" />
-                    <Line yAxisId="right" type="monotone" dataKey="revenue" stroke="#82ca9d" />
-                </LineChart>
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis yAxisId="left" />
+                        <YAxis yAxisId="right" orientation="right" />
+                        <Tooltip />
+                        <Legend />
+                        <Line yAxisId="left" type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line yAxisId="right" type="monotone" dataKey="sell" stroke="#82ca9d" />
+                        <Line yAxisId="right" type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                    </LineChart>
 
+
+                </div>
+                <div>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                        <Bar dataKey="sell" stackId="a" fill="#82ca9d" />
+                        <Bar dataKey="revenue" fill="#ffc658" />
+                    </BarChart>
+                </div>
             </div>
         </div>
     );
